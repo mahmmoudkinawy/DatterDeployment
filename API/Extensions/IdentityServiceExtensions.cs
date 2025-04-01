a2fb8c5da6ea74f136a0ef22dedf2e7d446a0337
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2025-04-01 18:14:44 +0200

    d

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -33,36 +33,6 @@ public static class IdentityServiceExtensions
                 }
             );
 
-        //services
-        //    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        //    .AddJwtBearer(options =>
-        //    {
-        //        var tokenKey = config["TokenKey"] ?? throw new Exception("TokenKey not found");
-        //        options.TokenValidationParameters = new TokenValidationParameters
-        //        {
-        //            ValidateIssuerSigningKey = true,
-        //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-        //            ValidateIssuer = false,
-        //            ValidateAudience = false
-        //        };
-
-        //        options.Events = new JwtBearerEvents
-        //        {
-        //            OnMessageReceived = context =>
-        //            {
-        //                var accessToken = context.Request.Query["access_token"];
-
-        //                var path = context.HttpContext.Request.Path;
-        //                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
-        //                {
-        //                    context.Token = accessToken;
-        //                }
-
-        //                return Task.CompletedTask;
-        //            }
-        //        };
-        //    });
-
         services
             .AddAuthorizationBuilder()
             .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"))
